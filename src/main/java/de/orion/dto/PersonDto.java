3e6c2c0a093fc326b095ba9180c1020995caa594
AIRAT1 <ayrat1@mail.ru>
2021-04-11 17:09:07 +0300

    add validation in dto

--- src/main/java/de/orion/dto/PersonDto.java
+++ src/main/java/de/orion/dto/PersonDto.java
@@ -5,14 +5,23 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class PersonDto {
     private String id;
+    @NotEmpty
+    @NotNull
     private String name;
+    @Positive
     private Short age;
+    @Email
     private String email;
     private AddressDto address;
 }
